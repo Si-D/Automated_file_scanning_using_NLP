feb214b3eb381d2ecf6df40eea01dda1559d60a1
Sharvesh222 <63151826+SharveshSubhash@users.noreply.github.com>
2020-12-14 14:48:02 +0530

    New_LeaseDateCalculation.py
    I have changed the total program to get the print the correct date after certain years mentioned by the User in the required "%d-%m-%y" format.

--- lease_calc.py
+++ lease_calc.py
@@ -1,6 +1,22 @@
 import datetime
-from dateutil.relativedelta import relativedelta
-
-x = datetime.datetime(2020,5,19) + relativedelta(years=+1) + relativedelta(days=+1)
-y = x.strftime('%d/%m/%Y')
-print(y)
+from datetime import date
+from datetime import timedelta
+def getDate():
+  date_entry = input()
+  day, month, year = map(int, date_entry.split('-'))
+  return datetime.date(year, month, day)
+da = getDate()
+yrs=int(input())
+def addYears(d, years):
+    try:
+#Return same day of the current year        
+        return d.replace(year = d.year + years)
+    except ValueError:
+#If not same day, it will return other, i.e.  February 29 to March 1 etc.        
+        return d + (date(d.year + years, 1, 1) - date(d.year, 1, 1))
+string=str(addYears(da,yrs))
+from datetime import datetime
+oldformat = string
+datetimeobject = datetime.strptime(oldformat,'%Y-%m-%d')
+newformat = datetimeobject.strftime('%d-%m-%Y')
+print(newformat)
